5acd0e3d0dabe90c9bb18011adf88d4690fd83da
Fly-Luck <hsdsmjay@gmail.com>
2015-03-10 02:01:43 +0800

    <version>v0.1.38</version> <author>Liu Junjie</author> <desc> 	modified:   project.properties 	modified:   res/layout/item_category.xml 	modified:   src/com/echobond/fragment/NewCategoryFragment.java 	modified:   src/com/echobond/util/JSONUtil.java </desc>

--- src/com/echobond/fragment/NewCategoryFragment.java
+++ src/com/echobond/fragment/NewCategoryFragment.java
@@ -11,30 +11,33 @@ import com.echobond.entity.Category;
 import com.echobond.intf.CategoryAsyncTaskCallback;
 import com.echobond.util.HTTPUtil;
 import com.echobond.util.JSONUtil;
-import com.google.gson.Gson;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.ShapeDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.View.OnClickListener;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
+import com.google.gson.reflect.TypeToken;
 
+/**
+ * 
+ * @author Ao Huijun
+ * @author Luck
+ *
+ */
 public class NewCategoryFragment extends Fragment implements CategoryAsyncTaskCallback{
 
-	private ArrayList<ShapeDrawable> bgList;
+	private ArrayList<Drawable> bgList;
 	private ListView categoryList;
 	
 	private CategoryInterface categorySelected;
-	private String categoryName = "";
 	
 	private ArrayList<Category> categories; 
 	
@@ -44,18 +47,18 @@ public class NewCategoryFragment extends Fragment implements CategoryAsyncTaskCa
 		
 		View categoryView = inflater.inflate(R.layout.fragment_new_post_category, container, false);
 		
-		bgList = new ArrayList<ShapeDrawable>();		
-		bgList.add((ShapeDrawable) getResources().getDrawable(R.drawable.corners_bg_blue));
-		bgList.add((ShapeDrawable) getResources().getDrawable(R.drawable.corners_bg_red));
-		bgList.add((ShapeDrawable) getResources().getDrawable(R.drawable.corners_bg_orange));
-		bgList.add((ShapeDrawable) getResources().getDrawable(R.drawable.corners_bg_yellow));
-		bgList.add((ShapeDrawable) getResources().getDrawable(R.drawable.corners_bg_cyan));
+		bgList = new ArrayList<Drawable>();
+		bgList.add((Drawable) getResources().getDrawable(R.drawable.corners_bg_blue));
+		bgList.add((Drawable) getResources().getDrawable(R.drawable.corners_bg_red));
+		bgList.add((Drawable) getResources().getDrawable(R.drawable.corners_bg_orange));
+		bgList.add((Drawable) getResources().getDrawable(R.drawable.corners_bg_green));
+		bgList.add((Drawable) getResources().getDrawable(R.drawable.corners_bg_yellow));
+		bgList.add((Drawable) getResources().getDrawable(R.drawable.corners_bg_cyan));
 		
 		categoryList = (ListView) categoryView.findViewById(R.id.list_category);
 		
 		new CategoryAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, CategoryAsyncTask.CATEGORY_LOAD,
 				HTTPUtil.getInstance().composePreURL(getActivity())+getResources().getString(R.string.url_load_categories), this);
-		
 		return categoryView;
 	}
 	
@@ -74,12 +77,15 @@ public class NewCategoryFragment extends Fragment implements CategoryAsyncTaskCa
 		}
 	}
 
+	@SuppressWarnings("unchecked")
+	@SuppressLint("NewApi")
 	@Override
 	public void onCategoryResult(JSONObject result) {
 		if(null == result){
 			Toast.makeText(getActivity(), "Failed loading categories", Toast.LENGTH_LONG).show();
 		} else {
-			categories = (ArrayList<Category>) JSONUtil.fromJSONToObject(result, ArrayList.class);
+			TypeToken<ArrayList<Category>> token = new TypeToken<ArrayList<Category>>(){};
+			categories = (ArrayList<Category>) JSONUtil.fromJSONToList(result, "categories", token);
 			ArrayList<HashMap<String, Object>> listItems = new ArrayList<HashMap<String, Object>>();
 			for(int i=0;i < categories.size(); i++){
 				HashMap<String, Object> map = new HashMap<String, Object>();
@@ -87,9 +93,21 @@ public class NewCategoryFragment extends Fragment implements CategoryAsyncTaskCa
 				listItems.add(map);
 			}
 			SimpleAdapter adapter = new SimpleAdapter(getActivity(), listItems, R.layout.item_category, new String[]{"category"}, new int[]{R.id.text_category});
-			for(int i=0;i<categoryList.getChildCount();i++){
-				categoryList.getChildAt(i);
-			}
+			categoryList.setAdapter(adapter);
+			categoryList.post(new Runnable() {
+				
+				@Override
+				public void run() {
+					for(int i=0;i<categoryList.getChildCount();i++){
+						ViewGroup view = (ViewGroup) categoryList.getChildAt(i);
+						int index = i;
+						if(index >= bgList.size()){
+							index = bgList.size()%index;
+						}
+						view.getChildAt(0).setBackground(bgList.get(index));
+					}
+				}
+			});
 		}
 	}
 }
--- src/com/echobond/util/JSONUtil.java
+++ src/com/echobond/util/JSONUtil.java
@@ -1,9 +1,14 @@
 package com.echobond.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class JSONUtil {
 	
@@ -22,7 +27,7 @@ public class JSONUtil {
 	public static JSONObject fromObjectToJSON(Object src){
 		JSONObject dest = new JSONObject();
 		try {
-			dest = new JSONObject(JSONUtil.fromObjectToString(src));
+			dest = new JSONObject(fromObjectToString(src));
 		} catch (JSONException e1){
 			e1.printStackTrace();
 		}
@@ -31,6 +36,19 @@ public class JSONUtil {
 	
 	public static Object fromJSONToObject(JSONObject obj, Class<?> cls){
 		return fromStringToObject(obj.toString(), cls);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static List fromJSONToList(JSONObject obj, String name, TypeToken<?> token){
+		JSONArray jArray = null;
+		try {
+			jArray = obj.getJSONArray(name);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		ArrayList list = gson.fromJson(jArray.toString(), token.getType());
+		return list;
+		
 	}
 	
 }
