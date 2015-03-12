015cf330c196d95d289b5c863e4f35b74c997fae
Fly-Luck <hsdsmjay@gmail.com>
2015-03-12 17:40:51 +0800

    <version>v0.1.46</version> <author>Luck</author> <desc>  Changes to be committed: 	modified:   src/com/echobond/util/ImageUtil.java </desc>

--- src/com/echobond/util/ImageUtil.java
+++ src/com/echobond/util/ImageUtil.java
@@ -1,6 +1,8 @@
 package com.echobond.util;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
@@ -8,6 +10,52 @@ import android.graphics.BitmapFactory;
 import android.util.Base64;
 
 public class ImageUtil {
+	
+	/**
+	 * compress bitmap
+	 * @param src
+	 * @param quality
+	 * @return
+	 */
+	public static Bitmap shrinkBitmap(Bitmap src, int quality){
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+		src.compress(CompressFormat.JPEG, quality, out);
+		Bitmap dest = BitmapFactory.decodeStream(in);
+		try {
+			if(null != out)
+				out.close();
+			if(null != in)
+				in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return dest;
+	}
+	
+	/**
+	 * resample bitmap
+	 * @param src
+	 * @param times
+	 * @return
+	 */
+	public static Bitmap resampleBitmap(Bitmap src, int times){
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+		src.compress(CompressFormat.PNG, 100, out);
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inSampleSize = times;
+		Bitmap dest = BitmapFactory.decodeStream(in, null, options);
+		try {
+			if(null != out)
+				out.close();
+			if(null != in)
+				in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return dest;
+	}
 	
 	/** 
 	 * Bitmap->Base64 String via Base32 
