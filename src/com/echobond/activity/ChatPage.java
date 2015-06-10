8e1c5738ed7941cfbd9b68af0a76bf67a5439dae
AO Huijun <daca.hjao@gmail.com>
2015-06-11 01:03:04 +0800

    <version>0.2.11</version> <author>AO Huijun</author> <desc> 	* DONE: the layout of ProfileFragment 	* DONE: the layout of ChatPage 	* UN-FINISH: the status of selecting groups / hashtags 	* UN-FINISH: the layout of search page 	* UN-FINISH: the function of ChatPage 	* UN-FINISH: the navigation from ProfileFragment to thoughts  Changes to be committed: 	modified:   AndroidManifest.xml 	modified:   res/drawable-hdpi/button_search.png 	modified:   res/drawable-hdpi/button_send.png 	modified:   res/drawable-hdpi/main_add_button.png 	new file:   res/drawable-hdpi/profile_about_desc.png 	new file:   res/drawable-hdpi/profile_about_dna.png 	new file:   res/drawable-hdpi/profile_about_earth.png 	new file:   res/drawable-hdpi/profile_about_heart.png 	new file:   res/drawable-hdpi/profile_about_language.png 	new file:   res/drawable-hdpi/profile_about_philosophy.png 	new file:   res/drawable-hdpi/profile_about_secrets.png 	new file:   res/drawable-hdpi/profile_about_tag.png 	new file:   res/drawable-hdpi/profile_about_todo.png 	new file:   res/drawable-hdpi/profile_about_trophy.png 	modified:   res/drawable-hdpi/setting_button.png 	modified:   res/layout/fragment_main_profile.xml 	new file:   res/layout/item_chat.xml 	modified:   res/layout/item_setting.xml 	modified:   res/layout/title_bar_main.xml 	modified:   res/layout/title_bar_search.xml 	modified:   res/layout/title_bar_setting.xml 	modified:   res/values/strings.xml 	modified:   src/com/echobond/activity/ChatPage.java 	modified:   src/com/echobond/fragment/ProfileFragment.java </desc>

--- src/com/echobond/activity/ChatPage.java
+++ src/com/echobond/activity/ChatPage.java
@@ -2,19 +2,55 @@ package com.echobond.activity;
 
 import com.echobond.R;
 
-import android.app.Activity;
+import android.app.ActionBar;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class ChatPage extends Activity {
+public class ChatPage extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_chat_page);
+		initActionBar();
+	}
+	
+	private void initActionBar() {
+		Toolbar chatToolbar = (Toolbar)findViewById(R.id.toolbar_chat_page);
+		setSupportActionBar(chatToolbar);
+		getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+		getSupportActionBar().setCustomView(R.layout.title_bar_setting);
+	}
+
+	public class ChatListAdapter extends CursorAdapter {
+
+		public ChatListAdapter(Context context, Cursor c, int flags) {
+			super(context, c, flags);
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public void bindView(View arg0, Context arg1, Cursor arg2) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public View newView(Context arg0, Cursor arg1, ViewGroup arg2) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
 	}
 	
 	@Override
--- src/com/echobond/fragment/ProfileFragment.java
+++ src/com/echobond/fragment/ProfileFragment.java
@@ -4,12 +4,12 @@ import com.echobond.R;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 /**
  * 
  * @author aohuijun
@@ -18,8 +18,22 @@ import android.widget.TextView;
 public class ProfileFragment extends Fragment {
 	
 	private ImageView profileFigureView;
-	private TextView profileTitle, profileBio;
-	private FragmentTabHost tabHost;
+	private TextView profileTitle, profileBio, profileGentle;
+	private TextView profileDNA, profileTrophy, profileTodo, profilePhilo, profileEarth, 
+					profileDesc, profileHeart, profileSec, profileLang, profileTag; 
+	private TextView viewThoughtsButton;
+	private String[] testString = {
+			"I speak Chinese! Have spent 5 years in Beijing.", 
+			"Have performed sexophone across India. Went to Grand Canyon!", 
+			"Set up my startup office in Silicon Valley. Visit Israel.", 
+			"You Only Live Once. Make it count. Be yourself and be nice.", 
+			"People stop talking about war but ART! No racism, everyone is a global citizen.", 
+			"Funny, care-free, music talent, nice, king of sexophone lol", 
+			"hiking, meet new fds, chinese food, cooking, sexophone", 
+			"didn’t know Santa Claus was a myth by 13 years old", 
+			"English, Hindi, Mandarin.", 
+			"#hkust #sexophone #india #beijing #mandarin #engineering #tech start up #hong kong #cooking #hiking #travelling #music #exchange #language exchange #party #chinese culture"
+	};
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -28,19 +42,43 @@ public class ProfileFragment extends Fragment {
 		profileFigureView = (ImageView)profileView.findViewById(R.id.profile_pic);
 		profileTitle = (TextView)profileView.findViewById(R.id.profile_title);
 		profileBio = (TextView)profileView.findViewById(R.id.profile_content);
+		profileGentle = (TextView)profileView.findViewById(R.id.profile_gender);
+		
+		profileDNA = (TextView)profileView.findViewById(R.id.profile_about_dna_text);
+		profileTrophy = (TextView)profileView.findViewById(R.id.profile_about_trophy_text);
+		profileTodo = (TextView)profileView.findViewById(R.id.profile_about_todo_text);
+		profilePhilo = (TextView)profileView.findViewById(R.id.profile_about_philo_text);
+		profileEarth = (TextView)profileView.findViewById(R.id.profile_about_earth_text);
+		profileDesc = (TextView)profileView.findViewById(R.id.profile_about_desc_text);
+		profileHeart = (TextView)profileView.findViewById(R.id.profile_about_heart_text);
+		profileSec = (TextView)profileView.findViewById(R.id.profile_about_sec_text);
+		profileLang = (TextView)profileView.findViewById(R.id.profile_about_lang_text);
+		profileTag = (TextView)profileView.findViewById(R.id.profile_about_tag_text);
+		
+		viewThoughtsButton = (TextView)profileView.findViewById(R.id.profile_view_thoughts);
 		
 		profileFigureView.setImageResource(R.drawable.default_avatar);
 		profileTitle.setText(getResources().getString(R.string.app_name));
 		profileBio.setText(getResources().getString(R.string.hello_world));
 		
-		tabHost = (FragmentTabHost)profileView.findViewById(R.id.profile_tabhost);
-		tabHost.setup(getActivity(), getChildFragmentManager(), R.id.profile_frame);
-
-		Bundle b1 = new Bundle(), b2 = new Bundle();
-		b1.putInt("Tab1", 1);
-		b2.putInt("Tab2", 2);
-		tabHost.addTab(tabHost.newTabSpec("about").setIndicator("About"), ProfileAboutFragment.class, b1);
-		tabHost.addTab(tabHost.newTabSpec("thoughts").setIndicator("Thoughts"), ProfileThoughtsFragment.class, b2);
+		profileDNA.setText(testString[0]);
+		profileTrophy.setText(testString[1]);
+		profileTodo.setText(testString[2]);
+		profilePhilo.setText(testString[3]);
+		profileEarth.setText(testString[4]);
+		profileDesc.setText(testString[5]);
+		profileHeart.setText(testString[6]);
+		profileSec.setText(testString[7]);
+		profileLang.setText(testString[8]);
+		profileTag.setText(testString[9]);
+		
+		viewThoughtsButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(getActivity().getApplicationContext(), "Navigate to ThoughtsFragment.class: TO BE DONE", Toast.LENGTH_SHORT).show();
+			}
+		});
 		
 		return profileView;
 		
