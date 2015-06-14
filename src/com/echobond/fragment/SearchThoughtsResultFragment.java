457c0163f53ce597fe42eb9c37bba31d73bd8406
AO Huijun <daca.hjao@gmail.com>
2015-06-14 23:31:51 +0800

    <version>0.2.24</version> <author>AO Huijun</author> <desc>  Changes to be committed: 	new file:   res/layout/fragment_search_result_people.xml 	new file:   res/layout/fragment_search_result_thoughts.xml 	modified:   src/com/echobond/fragment/SearchPeopleResultFragment.java 	modified:   src/com/echobond/fragment/SearchThoughtsResultFragment.java </desc>

--- src/com/echobond/fragment/SearchThoughtsResultFragment.java
+++ src/com/echobond/fragment/SearchThoughtsResultFragment.java
@@ -1,5 +1,7 @@
 package com.echobond.fragment;
 
+import com.echobond.R;
+
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
@@ -15,8 +17,9 @@ public class SearchThoughtsResultFragment extends ListFragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		return super.onCreateView(inflater, container, savedInstanceState);
+		View searchThoughtsResultView = inflater.inflate(R.layout.fragment_search_result_thoughts, container, false);
+		
+		return searchThoughtsResultView;
 	}
 	
 }
