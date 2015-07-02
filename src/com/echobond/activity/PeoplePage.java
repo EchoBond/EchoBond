9e28dbc5529f794a0cd3d8309287872834729bd4
AO Huijun <daca.hjao@gmail.com>
2015-07-02 21:32:54 +0800

    <version>0.2.80</version> <author>AO Huijun</author> <desc>  Changes to be committed: 	modified:   res/layout/activity_people_page.xml 	modified:   src/com/echobond/activity/PeoplePage.java </desc>

--- src/com/echobond/activity/PeoplePage.java
+++ src/com/echobond/activity/PeoplePage.java
@@ -2,10 +2,12 @@ package com.echobond.activity;
 
 import com.echobond.R;
 import com.echobond.entity.User;
+import com.echobond.fragment.ProfileFragment;
 
 import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -19,13 +21,13 @@ import android.widget.Toast;
  */
 public class PeoplePage extends ActionBarActivity {
 
-	private ImageView peopleFigureView;
+//	private ImageView peopleFigureView;
 	private TextView titleView;
-	private TextView peopleTitle, peopleBio, peopleGender;
-	private TextView peopleDNA, peopleTrophy, peopleTodo, peoplePhilo, peopleEarth, 
-					peopleDesc, peopleHeart, peopleSec, peopleLang, peopleTag, peopleGroup; 
-	private TextView viewThoughtsButton;
-	
+//	private TextView peopleTitle, peopleBio, peopleGender;
+//	private TextView peopleDNA, peopleTrophy, peopleTodo, peoplePhilo, peopleEarth, 
+//					peopleDesc, peopleHeart, peopleSec, peopleLang, peopleTag, peopleGroup; 
+//	private TextView viewThoughtsButton;
+	private ProfileFragment profileFragment;
 	private String userName;
 	
 	@Override
@@ -46,39 +48,47 @@ public class PeoplePage extends ActionBarActivity {
 		userName = getIntent().getStringExtra("userName");
 		titleView.setText(userName + "'s Profile");
 	}
-
+	
 	private void initContent() {
-		peopleFigureView = (ImageView)findViewById(R.id.people_pic);
-		peopleTitle = (TextView)findViewById(R.id.people_title);
-		peopleBio = (TextView)findViewById(R.id.people_content);
-		peopleGender = (TextView)findViewById(R.id.people_gender);
-		
-		peopleDNA = (TextView)findViewById(R.id.people_about_dna_text);
-		peopleTrophy = (TextView)findViewById(R.id.people_about_trophy_text);
-		peopleTodo = (TextView)findViewById(R.id.people_about_todo_text);
-		peoplePhilo = (TextView)findViewById(R.id.people_about_philo_text);
-		peopleEarth = (TextView)findViewById(R.id.people_about_earth_text); 
-		peopleDesc = (TextView)findViewById(R.id.people_about_desc_text);
-		peopleHeart = (TextView)findViewById(R.id.people_about_heart_text);
-		peopleSec = (TextView)findViewById(R.id.people_about_sec_text);
-		peopleLang = (TextView)findViewById(R.id.people_about_lang_text);
-		peopleTag = (TextView)findViewById(R.id.people_about_tag_text);
-		peopleGroup = (TextView)findViewById(R.id.people_about_group_text);
-		
-		viewThoughtsButton = (TextView)findViewById(R.id.people_view_thoughts);
-		viewThoughtsButton.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent();
-				intent.putExtra("userName", userName);
-				intent.setClass(PeoplePage.this, ThoughtsListPage.class);
-				startActivity(intent);
-				Toast.makeText(getApplicationContext(), "Thought List", Toast.LENGTH_SHORT).show();
-			}
-		});
-		
-		User user = new User();
+		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+		if (null == profileFragment) {
+			profileFragment = new ProfileFragment();
+		}
+		transaction.add(R.id.people_page_content, profileFragment).show(profileFragment).commit();
 	}
+	
+//	private void initContent() {
+//		peopleFigureView = (ImageView)findViewById(R.id.people_pic);
+//		peopleTitle = (TextView)findViewById(R.id.people_title);
+//		peopleBio = (TextView)findViewById(R.id.people_content);
+//		peopleGender = (TextView)findViewById(R.id.people_gender);
+//		
+//		peopleDNA = (TextView)findViewById(R.id.people_about_dna_text);
+//		peopleTrophy = (TextView)findViewById(R.id.people_about_trophy_text);
+//		peopleTodo = (TextView)findViewById(R.id.people_about_todo_text);
+//		peoplePhilo = (TextView)findViewById(R.id.people_about_philo_text);
+//		peopleEarth = (TextView)findViewById(R.id.people_about_earth_text); 
+//		peopleDesc = (TextView)findViewById(R.id.people_about_desc_text);
+//		peopleHeart = (TextView)findViewById(R.id.people_about_heart_text);
+//		peopleSec = (TextView)findViewById(R.id.people_about_sec_text);
+//		peopleLang = (TextView)findViewById(R.id.people_about_lang_text);
+//		peopleTag = (TextView)findViewById(R.id.people_about_tag_text);
+//		peopleGroup = (TextView)findViewById(R.id.people_about_group_text);
+//		
+//		viewThoughtsButton = (TextView)findViewById(R.id.people_view_thoughts);
+//		viewThoughtsButton.setOnClickListener(new View.OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				Intent intent = new Intent();
+//				intent.putExtra("userName", userName);
+//				intent.setClass(PeoplePage.this, ThoughtsListPage.class);
+//				startActivity(intent);
+//				Toast.makeText(getApplicationContext(), "Thought List", Toast.LENGTH_SHORT).show();
+//			}
+//		});
+//		
+//		User user = new User();
+//	}
 	
 }
