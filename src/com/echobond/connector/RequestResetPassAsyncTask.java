8802f05a7fa75e742c544b98d7ae91739c62b9c7
Fly-Luck <hsdsmjay@gmail.com>
2015-07-24 21:15:36 +0800

    <version>0.3.16</version> <author>Luck</author> <desc>  Changes to be committed: 	modified:   res/layout/activity_avatar_page.xml 	modified:   res/layout/fragment_dialog_select_avatar.xml 	modified:   res/values/server.xml 	modified:   src/com/echobond/activity/MainPage.java 	new file:   src/com/echobond/connector/RequestResetPassAsyncTask.java 	new file:   src/com/echobond/intf/RequestResetPassCallback.java </desc>

---
+++ src/com/echobond/connector/RequestResetPassAsyncTask.java
@@ -0,0 +1,42 @@
+package com.echobond.connector;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.echobond.entity.User;
+import com.echobond.intf.RequestResetPassCallback;
+import com.echobond.util.HTTPUtil;
+
+import android.os.AsyncTask;
+
+/**
+ * 
+ * This task is to handle pass reset request in server's DB.<br>
+ * Params (Object): user(User), url(String), activity(StartActivity)<br>
+ * Progress (Integer)<br>
+ * Result (JSONObject) <br>
+ * @author Luck
+ *
+ */
+public class RequestResetPassAsyncTask extends AsyncTask<Object, Integer, JSONObject> {
+	private RequestResetPassCallback activity;
+	@Override
+	protected JSONObject doInBackground(Object... params) {
+		User user = (User) params[0];
+		String url = (String) params[1];
+		activity = (RequestResetPassCallback) params[2];
+		JSONObject body = new JSONObject();
+		try {
+			body.put("user", user);
+		} catch (JSONException e1) {
+			e1.printStackTrace();
+		}
+		return HTTPUtil.getInstance().sendRequest(url, body, true);
+	}
+	
+	@Override
+	protected void onPostExecute(JSONObject result) {
+		super.onPostExecute(result);
+		activity.onRequestResetPassFinished(result);
+	}
+}
